07ce90f240a1f91fbbd52dc620852274338fd772
Ryan Yanke <rcyanke1@gmail.com>
2017-07-02 10:46:11 -0500

    Added a Utilities class that contains a method for printing out SQLException information.  This class can be extended to include methods for setting up the database.

---
+++ src/main/java/BaseballUtilities.java
@@ -0,0 +1,23 @@
+package src.main.java;
+
+import java.sql.SQLException;
+
+public class BaseballUtilities {
+
+    public static void printSQLException(SQLException ex) {
+      for (Throwable e : ex) {
+	if (e instanceof SQLException) {
+	  e.printStackTrace(System.err);
+	  System.err.println("SQLState: " + ((SQLException)e).getSQLState());
+	  System.err.println("Error Code: " + ((SQLException)e).getErrorCode());
+	  System.err.println("Message: " + e.getMessage());
+	  Throwable t = ex.getCause();
+	  while (t != null) {
+	    System.out.println("Cause: " + t);
+	    t = t.getCause();
+	  }
+	}
+      }
+    }
+
+} //end of class
