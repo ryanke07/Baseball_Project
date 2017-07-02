ab213f4a3caa4606ffb989eb8d535ac3298d9b6d
Ryan Yanke <rcyanke1@gmail.com>
2017-07-02 16:21:59 -0500

    Fixing the main frame.

--- src/main/java/BaseballUtilities.java
+++ src/main/java/BaseballUtilities.java
@@ -1,4 +1,3 @@
-package src.main.java;
 
 import java.sql.SQLException;
 
--- src/main/java/MainBaseballFrame.java
+++ src/main/java/MainBaseballFrame.java
@@ -1,5 +1,3 @@
-package main.src.java;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -9,34 +7,44 @@ public class MainBaseballFrame extends JFrame
   public MainBaseballFrame() {
     this.setSize(350,200);
     this.setLocation(200,200);
-    //When the user closes the window, the application will shut down.
+    //NOTE: adjust path !!!
+    String path =
+"/Users/dianeyanke/Documents/Baseball_Project/src/main/resources/";
+    String file = "baseballpic.jpg";
 
+    //When the user closes the window, the application will shut down.
     //TODO: add clean-up code to close connections to database
     //This will involve a window listener, see JDBC tutorial
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     //Add features
     JLabel titleOfApp = new JLabel("Fantasy Fantasy Baseball");
-    ImageIcon welcomePic = new ImageIcon("../resources/baseballpic.jpg");
+    JLabel welcomePic;
+    Image thePic = Toolkit.getDefaultToolkit().getImage(path + file);
     JButton loginButton = new JButton("Login");
     
-    //The pane to "glue" into the frame
     Container contentPane = getContentPane();
     contentPane.setLayout(new BorderLayout());
     //Add the components
+    titleOfApp.setFont(new Font("Courier New", Font.BOLD, 36));
+    titleOfApp.setForeground(Color.RED);
+    titleOfApp.setBackground(Color.WHITE);
     contentPane.add(titleOfApp, BorderLayout.NORTH);
-    //TODO: figure out how to get the image added
-    contentPane.add(loginButton, BorderLayout.SOUTH);
 
+    thePic = thePic.getScaledInstance(500, 300, Image.SCALE_DEFAULT);
+    welcomePic = new JLabel(new ImageIcon(thePic));
+    contentPane.add(welcomePic, BorderLayout.CENTER);
+
+    contentPane.add(loginButton, BorderLayout.SOUTH);
 
     //TODO: Either open a dialog for the button click or create a new frame
     loginButton.addActionListener(new ActionListener() {
-
-       String username = JOptionPane.showInputDialog(
-		MainBaseballFrame.this, "Username");
-    });
-    
-}
+       public void actionPerformed(ActionEvent e) {
+         String username = JOptionPane.showInputDialog(
+	        	MainBaseballFrame.this, "Username");
+       } //actionPerformed
+    }); //actionListener
+  } //constructor
 
   //Makes the frame visible.
   public void showFrame() {
